c06990f913afb0b1315aa8755fa2959698c2d6ac
Fred <leferpz@gmail.com>
2024-05-04 21:04:02 -0400

    I can now add tasks to any of the three stages

--- src/domstuff.js
+++ src/domstuff.js
@@ -1,19 +1,35 @@
 import { Task } from "./index.js";
 
-let toDoContainer = document.querySelector(".to-do-container");
-let toDoButton = document.querySelector(".to-do-button");
+// This gives me a list of the containers, which I can access with
+// index [0], [1], [2]
+let toDoContainerList = document.querySelectorAll(".to-do-container");
+
+// I'll do the same for the buttons
+let toDoButtonList = document.querySelectorAll(".to-do-button");
+
+// Let's do something similar with the input fields - I could do the same but I want to experiment!
 let toDoNameInput = document.querySelector("#to-do-name-input");
 let toDoDescInput = document.querySelector("#to-do-desc-input");
+let doingNameInput = document.querySelector("#doing-name-input");
+let doingDescInput = document.querySelector("#doing-desc-input");
+let doneNameInput = document.querySelector("#done-name-input");
+let doneDescInput = document.querySelector("#done-desc-input");
 
-function addToList(x) {
-    toDoContainer.appendChild(x)
+// Making my own lists of the nodes
+let nameInputList = [toDoNameInput, doingNameInput, doneNameInput];
+let descInputList = [toDoDescInput, doingDescInput, doneDescInput];
+
+// I want the addToList to add to a different container depending on its stage
+function addToList(x, stage) {
+    toDoContainerList[stage].appendChild(x)
 }
 
-toDoButton.addEventListener("click", () => {
-    event.preventDefault();
-    let test = new Task(toDoNameInput.value, toDoDescInput.value, 1);
-    let entry = document.createElement("div");
-    entry.setAttribute("style", "width: 90%; background: lightgray; margin: 10px 10px 10px 10px")
-    entry.innerHTML = test.evaluation;
-    addToList(entry);
-})
+for (let i = 0; i < toDoButtonList.length; i++) {
+    toDoButtonList[i].addEventListener("click", () => {
+        let test = new Task(nameInputList[i].value, descInputList[i].value, toDoButtonList[i]);
+        let entry = document.createElement("div");
+        entry.setAttribute("style", "width: 90%; background: lightgray; margin: 10px 10px 10px 10px")
+        entry.innerHTML = test.evaluation;
+        addToList(entry, i);
+        event.preventDefault();
+    })}
