712d638b131f72483b72d084772ec3b4157b5930
Fred <leferpz@gmail.com>
2024-05-04 21:50:56 -0400

    Defining

--- src/domstuff.js
+++ src/domstuff.js
@@ -7,6 +7,9 @@ let toDoContainerList = document.querySelectorAll(".to-do-container");
 // I'll do the same for the buttons
 let toDoButtonList = document.querySelectorAll(".to-do-button");
 
+// And the same for the new entry containers 
+let newEntryContainerList = document.querySelectorAll(".new-entries-container");
+
 // Let's do something similar with the input fields - I could do the same but I want to experiment!
 let toDoNameInput = document.querySelector("#to-do-name-input");
 let toDoDescInput = document.querySelector("#to-do-desc-input");
@@ -21,15 +24,45 @@ let descInputList = [toDoDescInput, doingDescInput, doneDescInput];
 
 // I want the addToList to add to a different container depending on its stage
 function addToList(x, stage) {
-    toDoContainerList[stage].appendChild(x)
+    newEntryContainerList[stage].appendChild(x)
 }
 
+// Setting up event listeners for each button, corresponding to the correct container and answers
 for (let i = 0; i < toDoButtonList.length; i++) {
     toDoButtonList[i].addEventListener("click", () => {
-        let test = new Task(nameInputList[i].value, descInputList[i].value, toDoButtonList[i]);
+
+        // Make a new task using the user's input
+        let toDo = new Task(nameInputList[i].value, descInputList[i].value, toDoButtonList[i]);
+
+        let toDoLine = document.createElement("div");
+        let leftBtn = document.createElement("button");
+        let removeBtn = document.createElement("button");
+        let rightBtn = document.createElement("button");
+
+        // Create a styled div with the evaluation as the text
         let entry = document.createElement("div");
         entry.setAttribute("style", "width: 90%; background: lightgray; margin: 10px 10px 10px 10px")
-        entry.innerHTML = test.evaluation;
-        addToList(entry, i);
+        entry.innerHTML = toDo.evaluation;
+
+        // Append the div to the corresponding place
+        function appendEntry() {
+            addToList(entry, i);
+            addToList(leftBtn, i);
+            addToList(removeBtn, i);
+            addToList(rightBtn, i);
+        }
+
+        function moveToRight() {
+            addToList(entry, i+1);
+            addToList(leftBtn, i+1);
+            addToList(removeBtn, i+1);
+            addToList(rightBtn, i+1);
+        }
+
+        rightBtn.addEventListener("click", moveToRight);
+        
+        appendEntry();
+        // Stop the page refreshing lol
         event.preventDefault();
     })}
+
